70580d47cb7d1ccda0afb9911aae9715e459e222
raphael <wapquinones28@gmail.com>
2019-11-14 22:02:59 +0800

    Realized I don't need selenium, I just needed to generate the url's and parse the page Can now parse pages but not perfectly

--- import_laws.py
+++ import_laws.py
@@ -1,22 +1,26 @@
 '''Use this to import all of the laws in the website locally'''
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+
 def gen_link(start_num):
-    start_num+=1
-    final_num=start_num+49
+    start_num += 1
+    final_num = start_num + 49
     return f"http://www.chanrobles.com/RepublicActs.{start_num}-{final_num}.html"
-#Access the list of laws using chrome
-driver = webdriver.Chrome("/home/raphael/chromedriver")
-#url = "http://www.chanrobles.com/RepublicActsmain.html"
-#driver.get(url)
-start_num=0
-final_num=0
-while start_num <= 9451: #used for R.A. 1 to 9451 because their intervals are 49
-    start_num=final_num+1
-    final_num=start_num+49
-    link = f"http://www.chanrobles.com/RepublicActs.{start_num}-{final_num}.html"
-    driver.get(link)
 
 
+# Access the list of laws using chrome
+#driver = webdriver.Chrome("/home/raphael/chromedriver")
 
-
-
+# Get R.A. 1-9500
+for i in range(3000,4000):
+    if i in range(1, 151):
+        link = f"http://www.chanrobles.com/RepublicActNo.{i}.html#.Xc05lVkzbeQ"
+    elif i in range(151, 9501):
+        link = f" http://www.chanrobles.com/republicacts/republicactno{i}.html"
+    #driver.get(link)
+    site = requests.get(link).text
+    html_site = BeautifulSoup(site,"html.parser")
+    parse = html_site.find("div",class_="post").find_all('p')
+    for str in parse:
+        print(str.text)
