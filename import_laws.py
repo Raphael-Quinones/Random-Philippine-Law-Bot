2cfe0a5a60266aa25df1b646f23b3ca8403cd925
raphael <wapquinones28@gmail.com>
2019-11-22 11:37:07 +0800

    Enabled Title cleaning (removes date from title)

--- import_laws.py
+++ import_laws.py
@@ -3,21 +3,32 @@ from bs4 import BeautifulSoup
 import requests
 
 num = 0
-success=0
+success = 0
 
-def gen_link(start_num):
-    start_num += 1
-    final_num = start_num + 49
-    return f"http://www.chanrobles.com/RepublicActs.{start_num}-{final_num}.html"
+
+def clean_title2(i, title):
+    # len("Republic Act. No. 1") is 18
+    # len("Republic Act. No. 21") is 19
+    # and so on...
+    length = len(str(i))
+    if length == 1:
+        new_title = title[:18]
+    elif length == 2:
+        new_title = title[:19]
+    elif length == 3:
+        new_title = title[:20]
+    return new_title
 
 
 # Generate Links
 # Get R.A. 1-9500
-for i in range(1, 2):
+for i in range(1, 100):
     if i in range(1, 151):
         link = f"http://www.chanrobles.com/RepublicActNo.{i}.html#.Xc05lVkzbeQ"
     elif i in range(151, 9501):
         link = f" http://www.chanrobles.com/republicacts/republicactno{i}.html"
+
+
     try:
         # driver.get(link)
         site = requests.get(link).text
@@ -26,21 +37,20 @@ for i in range(1, 2):
             'p')  # finds all paragraph (list object) in div class="post" not yet perfect but workable
         list = [str.text for str in parse]
         list.pop(0)  # removes first index which to let the first loop read the republic act
+        # Cleanse first line (title); removes dates
+        list[0] = clean_title2(i, list[0])
+
     except:
         continue
-    # removes errors, we can't do anything about those errors anyway
+        # removes errors, we can't do anything about those errors anyway
     try:
+        # click string exists when R.A. isn't there
         if "click" in list[0].lower():
             continue
         else:
-            with open("R.A. List\\"+list[0],"w") as file:
+            with open(list[0], "w") as file:
                 for line in list:
                     file.write(line + "\n")
-
-            #for line in list:
-            #    success+=1
-            #    print(success)
-
 
     except:
         continue
